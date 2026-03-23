40ca9353b4f76be66c2894e6ae789c98bda85dc2
Akshita Agarwal <163635159+akshitaagarwal26@users.noreply.github.com>
2026-03-24 01:13:10 +0530

    Update two_sum.cpp

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// Problem: Two Sum
+// Platform: LeetCode
+// Approach: Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (mp.find(need) != mp.end()) {
+                return {mp[need], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
