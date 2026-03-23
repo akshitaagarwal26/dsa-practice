3e012cfb247a0c87650c3c708f0f525af990c0cf
Akshita Agarwal <163635159+akshitaagarwal26@users.noreply.github.com>
2026-03-24 01:22:40 +0530

    Create reverse_linked_list.cpp

---
+++ LinkedList/reverse_linked_list.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Problem: Reverse Linked List
+// Approach: Iterative
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+};
