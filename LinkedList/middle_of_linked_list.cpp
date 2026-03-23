f7d42e6b84f4048496c29b4d7c397681e9ecd02d
Akshita Agarwal <163635159+akshitaagarwal26@users.noreply.github.com>
2026-03-24 01:23:11 +0530

    Create middle_of_linked_list.cpp

---
+++ LinkedList/middle_of_linked_list.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Problem: Middle of Linked List
+// Approach: Slow & Fast pointer
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
